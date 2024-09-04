471c55f8b18362a708efc56e6dc0fd32fecb31b4
Nathan Rothenberger <nrothenberger@activecampaign.com>
2024-09-04 14:49:21 -0500

    tailwind poc

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
 import { cx } from "class-variance-authority";
 import { Button } from "./components";
 
-const intents = [undefined, "primary", "secondary"] as const;
-const sizes = [undefined, "medium", "small"] as const;
+const variants = [
+  "fill-primary",
+  "fill-destructive",
+  "outline",
+  "text-primary",
+  "text-destructive",
+] as const;
+const sizes = ["medium", "small"] as const;
 
 function App() {
   return (
@@ -11,25 +17,25 @@ function App() {
         "relative",
         "h-max w-max",
         "self-center justify-self-center",
-        "[&_:where(th,td)]:p-2",
+        "[&_:where(th,td)]:p-2"
       )}
     >
       <thead>
         <tr>
           <th></th>
-          {intents.map((intent) => (
-            <th scope="col">{intent || "default"}</th>
+          {variants.map((variant) => (
+            <th scope="col">{variant}</th>
           ))}
         </tr>
       </thead>
       <tbody>
         {sizes.map((size) => (
           <tr>
-            <th scope="row">{size || "default"}</th>
-            {intents.map((intent) => (
+            <th scope="row">{size}</th>
+            {variants.map((variant) => (
               <td scope="col">
-                <Button {...(intent && { intent })} {...(size && { size })}>
-                  {intent || "default"} button
+                <Button {...(variant && { variant })} {...(size && { size })}>
+                  Camp Button
                 </Button>
               </td>
             ))}
@@ -41,3 +47,39 @@ function App() {
 }
 
 export default App;
+
+// import { Button } from "./components";
+
+// function App() {
+//   return (
+//     <table className="variant-table">
+//       <thead>
+//         <tr>
+//           <th></th>
+//           {variants.map((intent) => (
+//             <th scope="col">{intent || "default"}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {sizes.map((size) => (
+//           <tr>
+//             <th scope="row">{size}</th>
+//             {variants.map((variant) => (
+//               <td scope="col">
+//                 <Button
+//                   {...(variant && { variant: variant })}
+//                   {...(size && { size })}
+//                 >
+//                   Camp Button
+//                 </Button>
+//               </td>
+//             ))}
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// }
+
+// export default App;
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,33 +1,81 @@
 import React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
-const button = cva("button", {
-  variants: {
-    intent: {
-      primary: [
-        "bg-blue-500",
-        "text-white",
-        "border-transparent",
-        "hover:bg-blue-600",
-      ],
-      secondary: [
-        "bg-white",
-        "text-gray-800",
-        "border-gray-400",
-        "hover:bg-gray-100",
-      ],
+/**
+ *
+ * Reset + Base
+ *
+ */
+
+const _reset =
+  "appearance-none box-border inline-block m-0 overflow-visible normal-case";
+
+const _defaultBox =
+  "cursor-pointer inline-flex justify-center items-center gap-1";
+const _defaultBorder = "border border-solid rounded"; // should be able to set border here;
+const _defaultTypography = "family-sans font-medium text-sm ";
+const _defaultTransition =
+  "duration-[400ms] ease-camp-out transition-[background] transition-[border]";
+const _defaultDisabled = "cursor-not-allowed";
+
+/**
+ *
+ * Variants
+ *
+ */
+
+// Fill
+const _fill =
+  "text-text-on-emphasis fill-icon-decorative-on-emphasis border-transparent disabled:opacity-35";
+const _fillPrimary =
+  "bg-bg-primary-emphasis hover:bg-bg-primary-emphasis-hover active:bg-bg-primary-emphasis-active";
+const _fillDestructive =
+  "bg-bg-destructive-emphasis hover:bg-bg-destructive-emphasis-hover active:bg-bg-destructive-emphasis-active";
+
+// Outline
+const _outline =
+  "border-border-supportive text-text-default bg-bg-default hover:bg-bg-primary-hover active:bg-bg-active disabled:opacity-5";
+
+// Text
+const _text = "bg-transparent border-transparent disabled:opacity-5";
+const _textPrimary =
+  "text-text-primary hover:bg-bg-primary-hover active:bg-bg-primary-active";
+const _textDestructive =
+  "text-text-destructive hover:bg-bg-destructive-hover active:bg-bg-destructive-active";
+
+const _small = "py-1 px-2 h-[28px]";
+const _medium = "py-2 px-3 h-[36px]";
+
+const button = cva(
+  [
+    "button",
+    _reset,
+    _defaultBox,
+    _defaultBorder,
+    _defaultTypography,
+    _defaultTransition,
+    _defaultDisabled,
+  ],
+  {
+    variants: {
+      variant: {
+        "fill-primary": [_fill, _fillPrimary],
+        "fill-destructive": [_fill, _fillDestructive],
+        outline: _outline,
+        "text-primary": [_text, _textPrimary],
+        "text-destructive": [_text, _textDestructive],
+      },
+      size: {
+        small: _small,
+        medium: _medium,
+      },
     },
-    size: {
-      small: ["text-sm", "py-1", "px-2"],
-      medium: ["text-base", "py-2", "px-4"],
+    defaultVariants: {
+      variant: "fill-primary",
+      size: "medium",
     },
-  },
-  compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
-  defaultVariants: {
-    intent: "primary",
-    size: "medium",
-  },
-});
+  }
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -35,7 +83,7 @@ export interface ButtonProps
 
 export const Button: React.FC<ButtonProps> = ({
   className,
-  intent,
+  variant,
   size,
   ...props
-}) => <button className={button({ intent, size, className })} {...props} />;
+}) => <button className={button({ variant, size, className })} {...props} />;
